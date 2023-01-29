b71c0529b5db4225ccce0f8d3765ae5de32d0583
Eliás Yousef <54670498+eliasyousef00@users.noreply.github.com>
2023-01-29 18:24:12 -0300

    Adicionando exemplo de teste

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+# EXEMPLO DE TESTE
+
+import unittest
+
+def soma(a, b):
+    return a + b
+
+class TestSoma(unittest.TestCase):
+    def test_soma(self):
+        self.assertEqual(soma(1, 2), 3)
+        self.assertEqual(soma(-1, -2), -3)
+        self.assertEqual(soma(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
