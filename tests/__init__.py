a9f638c6c607c263544e23016361445cc9a8b974
Eliás Yousef <eliasyousef00@gmail.com>
2023-01-29 21:17:35 +0000

    29

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,13 @@
-def test_foo_bar():
-   assert True
+import unittest
+
+def soma(a, b):
+    return a + b
+
+class TestSoma(unittest.TestCase):
+    def test_soma(self):
+        self.assertEqual(soma(1, 2), 3)
+        self.assertEqual(soma(-1, -2), -3)
+        self.assertEqual(soma(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
